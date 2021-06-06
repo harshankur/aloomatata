c3bd90f71a655eea2fd199bdd150b2b6fcec1350
Harsh Ankur <harshankur@outlook.com>
2021-06-06 13:44:54 +0530

    [add] include range api completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
-const fs        = require('fs');
-const express   = require('express');
-const scissors  = require('scissors');
+const fs            = require('fs');
+const express       = require('express');
+const scissors      = require('scissors');                  // pdf editor oss
+const fileUpload    = require('express-fileupload')         // handles file uploads
 
+const app           = express()
+
+app.use(express.json());
+app.use(fileUpload());
 
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -9,7 +14,37 @@ app.use(function (req, res, next) {
 	next();
 });
 
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
 
 
+app.get('/api/v1/checkConnection', (req, res) => {
+    res.send("Connection OK");
+})
+
+app.post('/api/v1/split/include/range', async (req, res) => {
+    var inputFile           = await req.files.file;
+    var inputFileLocation   = './input/' + inputFile.name;
+    await inputFile.mv(inputFileLocation);
+
+    var inputRangeFrom      = req.body.from;
+    var inputRangeTo        = req.body.to;
+
+    var outputFileName      = `ir_${inputFile.name}`;
+    var outputFileLocation  = `./output/ir_${inputFile.name}`;
+
+    scissors(inputFileLocation)
+    .range(inputRangeFrom, inputRangeTo)
+    .pdfStream()
+    .pipe(fs.createWriteStream(outputFileLocation))
+    .on('finish', function(){
+      console.log("Successful");
+      res.download(outputFileLocation, outputFileName);
+    }).on('error',function(err){
+      throw err;
+    });
+})
+
+
+
+//Server listening at port
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Aloomatata listening on port ${PORT}!`));
